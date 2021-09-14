32e4fc7bd6d45380b0836be99ec9586dfbe495c3
Chris Stewart <c.stewart5545@gmail.com>
2021-09-14 15:26:40 -0500

    Base styles, game logic

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
+const squares = [...document.querySelectorAll('.square')];
+const reset = document.querySelector('.reset');
 
 
-const playerFactory = (name,team,turn)=> {
-return {name,team,turn};
-}
 
-const gameBoard = () => {
-    let squares = document.querySelectorAll('.square');
-    console.log(squares)
-    return squares;    
-};
+const gameBoard = (() => {
+    const playerFactory = (name,team,turn)=> {
+    return {name,team,turn};
+    }
 
-const game = (() => {
     const player1 = playerFactory("P1","X",true)
     const player2 = playerFactory("P2","O",false)
 
-    //creat game board
-    gameBoard();
-    console.log(gameBoard[1])
+    //create game board
+    let board = [];
+    
     ///making a move
+    const makeMove = (e) => {
+        if(player1.turn && e.target.textContent === ""){
+            e.target.textContent = player1.team;
+            player1.turn= !player1.turn;
+            player2.turn= !player2.turn;            
+        } else if(player2.turn && e.target.textContent === ""){
+            e.target.textContent = player2.team;
+            player1.turn= !player1.turn;
+            player2.turn= !player2.turn;  
+        } else return;
+        updateBoardArr();
+    } 
+    // Update Game Board Array
+    const updateBoardArr = () => {
+        let update = squares.map(content=>content.textContent)
+        board = update;
+        console.log(board)
+    }
 
+    // Check for a win
+
+    // reset board
+    reset.addEventListener('click',()=>{
+        squares.forEach(square=>square.textContent = "")
+        player1.turn= true;
+        player2.turn= false;
+        board = [];
+    })
+    
+
+    squares.forEach(square=>square.addEventListener('click',e=>makeMove(e)))
 })();
 
 
